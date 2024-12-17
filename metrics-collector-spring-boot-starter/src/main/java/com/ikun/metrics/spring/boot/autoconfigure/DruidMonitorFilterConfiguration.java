2d046de2fe961cc35cfe6fef68c348a75acedd14
ikun <2023266508@qq.com>
2024-12-17 20:12:47 +0800

    1217

--- metrics-collector-spring-boot-starter/src/main/java/com/ikun/metrics/spring/boot/autoconfigure/DruidMonitorFilterConfiguration.java
+++ metrics-collector-spring-boot-starter/src/main/java/com/ikun/metrics/spring/boot/autoconfigure/DruidMonitorFilterConfiguration.java
@@ -1,28 +1,71 @@
 package com.ikun.metrics.spring.boot.autoconfigure;
 
+import com.alibaba.druid.filter.Filter;
+import com.alibaba.druid.filter.FilterChainImpl;
+import com.alibaba.druid.pool.DruidAbstractDataSource;
 import com.alibaba.druid.pool.DruidDataSource;
 import com.ikun.metrics.transaction.collector.DruidTransactionNoneCommitCollector;
 import com.ikun.metrics.transaction.holder.TransactionMetricsHolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
+import javax.sql.DataSource;
+import java.lang.reflect.Field;
 import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 
 @Configuration
 @AutoConfigureAfter(MetricsCollectorAutoConfiguration.class)
-@ConditionalOnClass(DruidDataSource.class)
-public class DruidMonitorFilterConfiguration {
+public class DruidMonitorFilterConfiguration implements InitializingBean {
 
-    @Bean
-    @ConditionalOnMissingBean
-    @ConditionalOnBean(DruidDataSource.class)
-    public DruidTransactionNoneCommitCollector druidTransactionNoneCommitCollector(DruidDataSource dataSource, TransactionMetricsHolder holder) {
-        DruidTransactionNoneCommitCollector collector = new DruidTransactionNoneCommitCollector(holder);
-        dataSource.setProxyFilters(Collections.singletonList(collector));
-        return collector;
+    private static final Logger log = LoggerFactory.getLogger(DruidMonitorFilterConfiguration.class);
+
+    private final ObjectProvider<TransactionMetricsHolder> holderObjectProvider;
+    private final DataSource dataSource;
+
+    public DruidMonitorFilterConfiguration(ObjectProvider<TransactionMetricsHolder> holderObjectProvider, DataSource dataSource) {
+        this.holderObjectProvider = holderObjectProvider;
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        TransactionMetricsHolder metricsHolder = holderObjectProvider.getIfAvailable();
+        if (metricsHolder != null) {
+            DruidTransactionNoneCommitCollector collector = new DruidTransactionNoneCommitCollector(metricsHolder);
+            setProxyFilters(dataSource, collector);
+        }
+    }
+
+    private void setProxyFilters(DataSource dataSource, Filter filter) {
+        if (dataSource instanceof DruidDataSource) {
+            ((DruidDataSource) dataSource).setProxyFilters(Collections.singletonList(filter));
+            resetDruidDataSourceFilterChain(dataSource);
+        }
+        else if (dataSource instanceof AbstractRoutingDataSource) {
+            Map<Object, DataSource> resolvedDataSources = ((AbstractRoutingDataSource) dataSource).getResolvedDataSources();
+            resolvedDataSources.values().forEach(v -> setProxyFilters(v, filter));
+        }
+    }
+
+    private void resetDruidDataSourceFilterChain(DataSource dataSource) {
+        if (dataSource instanceof DruidDataSource) {
+            Class<? extends DataSource> sourceClass = dataSource.getClass();
+            try {
+                Field filterChain = sourceClass.getDeclaredField("filterChain");
+                filterChain.setAccessible(true);
+                filterChain.set(dataSource, null);
+            } catch (NoSuchFieldException e) {
+                log.warn("field filterChain not exist class {}", sourceClass.getName(), e);
+            } catch (IllegalAccessException e) {
+                log.warn("field filterChain cannot access in class {}", sourceClass.getName(), e);
+            }
+        }
     }
 }
--- metrics-collector/src/main/java/com/ikun/metrics/transaction/collector/mysql/query/MySQLTrxQuerys.java
+++ metrics-collector/src/main/java/com/ikun/metrics/transaction/collector/mysql/query/MySQLTrxQuerys.java
@@ -6,25 +6,26 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Timestamp;
+import java.sql.*;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MySQLTrxQuerys {
 
-    private static final String TRX_LOCK_QUERY_SQL = " select * from information_schema.INNODB_TRX T1 left join information_schema.INNODB_LOCKS T2 on T1.trx_id = T2.lock_trx_id where T1.trx_id = ? ";
+    private static final String TRX_LOCK_QUERY_SQL_5 = " select * from information_schema.INNODB_TRX T1 left join information_schema.INNODB_LOCKS T2 on T1.trx_id = T2.lock_trx_id where T1.trx_id = ? ";
+    private static final String TRX_LOCK_QUERY_SQL_8 = " select * from information_schema.INNODB_TRX T1 left join performance_schema.data_locks T2 on T1.trx_id = T2.ENGINE_LOCK_ID where T1.trx_id = ? ";
 
     public static final String TRX_MODEL_KEY = "TRX_MODEL";
     private static final Logger log = LoggerFactory.getLogger(MySQLTrxQuerys.class);
 
+    private static final Map<DataSource, MySQLVersion> MY_SQL_VERSION_MAP = new HashMap<>();
+
     public static TransactionModel queryRunningTrxModel(DataSource dataSource, Long trxId) {
         try(Connection connection = dataSource.getConnection()) {
-            try(PreparedStatement ps = connection.prepareStatement(TRX_LOCK_QUERY_SQL, ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY)) {
+            MySQLVersion version = getMySQLVersion(dataSource);
+            try(PreparedStatement ps = connection.prepareStatement(determineSql(dataSource), ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY)) {
                 ps.setLong(1, trxId);
-                try(ResultSet rs = ps.executeQuery(TRX_LOCK_QUERY_SQL)) {
+                try(ResultSet rs = ps.executeQuery()) {
                     Map<Object, TransactionModel> joinMergeMap = new HashMap<>();
                     while (rs.next()) {
                         Object trxIdVal = rs.getObject("trx_id");
@@ -47,10 +48,10 @@ public class MySQLTrxQuerys {
                             joinMergeMap.put(trxIdVal, model);
                         }
                         LockModel lockModel = new LockModel();
-                        lockModel.setLockId(rs.getString("lock_id"));
-                        lockModel.setLockIndex(rs.getString("lock_index"));
-                        lockModel.setLockData(rs.getString("lock_data"));
-                        lockModel.setLockTable(rs.getString("lock_table"));
+                        lockModel.setLockId(rs.getString(version == MySQLVersion.VERSION_5 ? "lock_id" : "ENGINE_LOCK_ID"));
+                        lockModel.setLockIndex(rs.getString(version == MySQLVersion.VERSION_5 ? "lock_index" : "INDEX_NAME"));
+                        lockModel.setLockData(rs.getString(version == MySQLVersion.VERSION_5 ? "lock_data" : "LOCK_DATA"));
+                        lockModel.setLockTable(rs.getString(version == MySQLVersion.VERSION_5 ? "lock_table" : "OBJECT_NAME"));
                         lockModel.setTransactionId(Long.parseLong(trxIdVal.toString()));
                         joinMergeMap.get(trxIdVal).getLocks().add(lockModel);
                     }
@@ -63,8 +64,45 @@ public class MySQLTrxQuerys {
         return null;
     }
 
-//    public static List<Map<String, Object>> convert(ResultSet rs) {
-//
-//    }
+    public static String determineSql(DataSource dataSource) {
+        MySQLVersion version = getMySQLVersion(dataSource);
+        if (version == MySQLVersion.VERSION_5) {
+            return TRX_LOCK_QUERY_SQL_5;
+        } else {
+            return TRX_LOCK_QUERY_SQL_8;
+        }
+    }
+
+    public static MySQLVersion getMySQLVersion(DataSource dataSource) {
+        MySQLVersion version = MY_SQL_VERSION_MAP.get(dataSource);
+        if (version == null) {
+            try(Connection conn = dataSource.getConnection()) {
+                try(Statement st = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY)) {
+                    try(ResultSet rs = st.executeQuery(" select version() ")) {
+                        while (rs.next()) {
+                            String v = rs.getString(1);
+                            if (v != null && v.startsWith("5")) {
+                                version = MySQLVersion.VERSION_5;
+                            } else if (v != null && v.startsWith("8")) {
+                                version = MySQLVersion.VERSION_8;
+                            }
+                            if (version != null) {
+                                MY_SQL_VERSION_MAP.put(dataSource, version);
+                            }
+                        }
+                        log.info(" current datasource version: {}", version);
+                    }
+                }
+            } catch (SQLException e) {
+                log.error("获取连接异常", e);
+                version = MySQLVersion.VERSION_5;
+            }
+        }
+        return version;
+    }
+
+    public enum MySQLVersion {
+        VERSION_5, VERSION_8
+    }
 
 }
